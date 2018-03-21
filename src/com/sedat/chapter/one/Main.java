b0b02248253fe0289f60db6bf406f3bedd7df50a
Sedat Camli <sedatcamli@live.com>
2018-03-22 01:08:32 +0300

    adding main method

---
+++ src/com/sedat/chapter/one/Main.java
@@ -0,0 +1,11 @@
+package com.sedat.chapter.one;
+
+//        owner:sedatcamli
+//        17.03.2018
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Main");
+    }
+}
