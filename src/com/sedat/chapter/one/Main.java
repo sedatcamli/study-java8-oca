b88b0705813d77da1ca16e2a2cc551249e917638
Sedat Camli <sedatcamli@live.com>
2018-03-21 23:18:31 +0300

    sample 1 add main method with String[] args

--- src/com/sedat/chapter/one/Main.java
+++ src/com/sedat/chapter/one/Main.java
@@ -3,5 +3,10 @@ package com.sedat.chapter.one;
 //        owner:sedatcamli
 //        17.03.2018
 
-public class Main {
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("main");
+    }
+
 }
